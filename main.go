ba9874126af80104fc72865539412fabb41fb2ac
Leo <leowzukw@171819.xyz>
2018-10-21 10:29:07 +0200

    Refactor a bit to prepare command line flags

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	// "flag"
 	"fmt"
 	"log"
 	"os"
@@ -8,38 +9,61 @@ import (
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-func main() {
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("TLGCLI_TOKEN"))
+// Create a bot with token
+func login(token string) (bot *tgbotapi.BotAPI) {
+	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		log.Panic(err)
 	}
+	return
+}
 
-	bot.Debug = true
+// Type of functions handling updates
+type handler func(*tgbotapi.BotAPI, tgbotapi.Update)
 
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-
+// Get updates continuously for a bot and pass it to a handler
+func updateLoop(bot *tgbotapi.BotAPI, h handler) {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
 	updates, err := bot.GetUpdatesChan(u)
 
-	for update := range updates {
-		if update.Message == nil { // ignore any non-Message Updates
-			continue
-		}
-
-		chatID := update.Message.Chat.ID
-
-		log.Printf("[%s(%s)] %s", update.Message.From.UserName, chatID, update.Message.Text)
-
-		msgText := fmt.Sprint("chatID: ", chatID)
-
-		msg := tgbotapi.NewMessage(chatID, msgText)
-		// msg.ReplyToMessageID = update.Message.MessageID
-
-		_, err := bot.Send(msg)
-		if err != nil {
-			log.Panic("Error sending message: ", msgText)
-		}
+	if err != nil {
+		log.Panic("Error getting updates")
 	}
+
+	for update := range updates {
+		h(bot, update)
+	}
+}
+
+// Message handler replying by giving current chat ID
+func replyChatID(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+	if update.Message == nil { // ignore any non-Message Updates
+		return
+	}
+
+	chatID := update.Message.Chat.ID
+
+	log.Printf("[%s(%s)] %s", update.Message.From.UserName, chatID, update.Message.Text)
+
+	msgText := fmt.Sprint("chatID: ", chatID)
+
+	msg := tgbotapi.NewMessage(chatID, msgText)
+	// msg.ReplyToMessageID = update.Message.MessageID
+
+	_, err := bot.Send(msg)
+	if err != nil {
+		log.Panic("Error sending message: ", msgText)
+	}
+}
+
+func main() {
+	bot := login(os.Getenv("TLGCLI_TOKEN"))
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	updateLoop(bot, replyChatID)
 }
